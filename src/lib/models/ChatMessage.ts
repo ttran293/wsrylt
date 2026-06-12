f91d2433fb3f363f2a4297b0a9cb99a2ac76f950
Thanh Nam Tran <ttran19@umbc.edu>
2026-06-11 23:04:26 -0400

    message store upd

--- src/lib/models/ChatMessage.ts
+++ src/lib/models/ChatMessage.ts
@@ -19,6 +19,14 @@ export type IChatMessage = InferSchemaType<typeof ChatMessageSchema> & {
   _id: mongoose.Types.ObjectId;
 };
 
+const existingChatMessageModel = mongoose.models.ChatMessage as
+  | Model<IChatMessage>
+  | undefined;
+
+if (existingChatMessageModel && !existingChatMessageModel.schema.path("bodyRaw")) {
+  mongoose.deleteModel("ChatMessage");
+}
+
 export const ChatMessage: Model<IChatMessage> =
-  mongoose.models.ChatMessage ??
+  (mongoose.models.ChatMessage as Model<IChatMessage> | undefined) ??
   mongoose.model<IChatMessage>("ChatMessage", ChatMessageSchema);
